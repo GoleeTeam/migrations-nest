f43a1cca51de47d2f3bda29c6eb960798a4d122d
Luca Giovenzana <luca@giovenzana.org>
2023-01-19 11:54:30 +0100

    feat(paginate): support initialSkip

--- src/module/interfaces/paginate.spec.ts
+++ src/module/interfaces/paginate.spec.ts
@@ -1,44 +1,51 @@
-import { paginate } from './paginate';
+import { paginate } from "./paginate";
 
-describe('cronjob paginate function', () => {
-	const func = jest.fn();
+describe("cronjob paginate function", () => {
+  const func = jest.fn();
 
-	afterEach(async () => {
-		jest.resetAllMocks();
-	});
+  afterEach(async () => {
+    jest.resetAllMocks();
+  });
 
-	it('should paginate correctly 0 count, limit 10', async () => {
-		await paginate({ count: 0, limit: 10 }, func);
+  it("should paginate correctly 0 count, limit 10", async () => {
+    await paginate({ count: 0, limit: 10 }, func);
 
-		expect(func).toBeCalledTimes(0);
-	});
+    expect(func).toBeCalledTimes(0);
+  });
 
-	it('should paginate correctly 5 count, limit 10', async () => {
-		await paginate({ count: 5, limit: 10 }, func);
+  it("should paginate correctly 5 count, limit 10", async () => {
+    await paginate({ count: 5, limit: 10 }, func);
 
-		expect(func).toBeCalledTimes(1);
-	});
+    expect(func).toBeCalledTimes(1);
+  });
 
-	it('should paginate correctly 10 count, limit 10', async () => {
-		await paginate({ count: 10, limit: 10 }, func);
+  it("should paginate correctly 10 count, limit 10", async () => {
+    await paginate({ count: 10, limit: 10 }, func);
 
-		expect(func).toBeCalledTimes(1);
-	});
+    expect(func).toBeCalledTimes(1);
+  });
 
-	it('should paginate correctly 42 count, limit 10', async () => {
-		await paginate({ count: 42, limit: 10 }, func);
+  it("should paginate correctly 42 count, limit 10", async () => {
+    await paginate({ count: 42, limit: 10 }, func);
 
-		expect(func).toBeCalledTimes(5);
-		expect(func.mock.calls[0]).toEqual([0, 10]);
-		expect(func.mock.calls[1]).toEqual([10, 10]);
-		expect(func.mock.calls[2]).toEqual([20, 10]);
-		expect(func.mock.calls[3]).toEqual([30, 10]);
-		expect(func.mock.calls[4]).toEqual([40, 10]);
-	});
+    expect(func).toBeCalledTimes(5);
+    expect(func.mock.calls[0]).toEqual([0, 10]);
+    expect(func.mock.calls[1]).toEqual([10, 10]);
+    expect(func.mock.calls[2]).toEqual([20, 10]);
+    expect(func.mock.calls[3]).toEqual([30, 10]);
+    expect(func.mock.calls[4]).toEqual([40, 10]);
+  });
 
-	it('should paginate correctly 42 count, limit 100', async () => {
-		await paginate({ count: 42, limit: 100 }, func);
+  it("should paginate correctly 42 count, limit 100", async () => {
+    await paginate({ count: 42, limit: 100 }, func);
 
-		expect(func).toBeCalledTimes(1);
-	});
+    expect(func).toBeCalledTimes(1);
+  });
+
+  it("should start from initialSkip", async function () {
+    await paginate({ count: 200, limit: 10, initialSkip: 100 }, func);
+
+    expect(func).toBeCalledTimes(10);
+    expect(func.mock.calls[0]).toEqual([100, 10]);
+  });
 });
--- src/module/interfaces/paginate.ts
+++ src/module/interfaces/paginate.ts
@@ -1,8 +1,12 @@
 export const paginate = async (
-	param: { count: number; limit: number },
-	func: (skip: number, limit: number) => Promise<void>
+  param: { count: number; limit: number; initialSkip?: number },
+  func: (skip: number, limit: number) => Promise<void>
 ) => {
-	for (let skip = 0; skip < param.count; skip += param.limit) {
-		await func(skip, param.limit);
-	}
+  for (
+    let skip = param.initialSkip || 0;
+    skip < param.count;
+    skip += param.limit
+  ) {
+    await func(skip, param.limit);
+  }
 };
