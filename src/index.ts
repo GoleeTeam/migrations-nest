21ceb4644235bdc57708abbd89e1ce542ec4f241
Luca Giovenzana <luca@giovenzana.org>
2024-09-09 17:18:49 +0200

    fix: missing MigrationOptions export

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { MigrationsModule } from './module/migrations.module';
 import { IMigrationScript } from './module/interfaces/migration-script.interface';
 import { paginate } from './module/interfaces/paginate';
 import { MigrationScript } from './module/interfaces/migration-script.abstract';
+import { MigrationsOptions } from './module/options';
 
-export { MigrationsModule, IMigrationScript, MigrationScript, paginate };
+export { MigrationsModule, IMigrationScript, MigrationScript, paginate, MigrationsOptions };
