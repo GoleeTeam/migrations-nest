3ea501dbda7efe9cdb2399660cc2d25339bbaaf3
Jacopo Franzoi <jacopo@golee.it>
2025-08-04 18:54:06 +0200

    refactor: encapsulated unused exported code

--- src/module/migrations.module.ts
+++ src/module/migrations.module.ts
@@ -5,7 +5,7 @@ import { MigrationsScriptsProvider } from './migrations-scripts.provider';
 import { MigrationVersionRepo } from './repo/migration-version.repo';
 import { MongoClient } from 'mongodb';
 
-export const MigrationScriptFactory = (options: Pick<MigrationsOptions, 'scripts'>) => {
+const MigrationScriptFactory = (options: Pick<MigrationsOptions, 'scripts'>) => {
     return {
         provide: MigrationsScriptsProvider,
         useFactory: (...args: any[]) => {
@@ -27,8 +27,6 @@ const MigrationVersionRepoFactory = (options: Pick<MigrationsOptions, 'mongoClie
 
 @Module({})
 export class MigrationsModule {
-    private static MIGRATIONS_MODULE_OPTIONS_TOKEN = 'MIGRATIONS_MODULE_OPTIONS';
-
     static forRoot(options: MigrationsOptions): DynamicModule {
         const providers = [
             MigrationsRunner,
